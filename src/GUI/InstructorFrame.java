d19978a1a3ff7a70351aa648604bc0291b35cefc
Hayri Durmaz <hayri.durmaz@tedu.edu.tr>
2017-12-28 02:33:19 +0300

    Gui starter code added

---
+++ src/GUI/InstructorFrame.java
@@ -0,0 +1,42 @@
+package GUI;
+
+import java.awt.event.ActionListener;
+import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+
+import Model.Instructor;
+
+public class InstructorFrame extends JFrame {
+
+	private Instructor instructor;
+
+	public InstructorFrame(Instructor instructor) {
+		this.instructor = instructor;
+		init();
+	}
+
+	private void init() {
+		this.setSize(800, 600);
+		this.setLocationRelativeTo(null);
+
+		JTabbedPane tabbedPane = new JTabbedPane();
+
+		// You won't use this particular code.
+		// You need to modify it
+		// Just use it as an example to create course tabbedpanes.
+		JPanel panel1 = new JPanel();
+		JPanel panel2 = new JPanel();
+		tabbedPane.addTab("Panel 1", panel1);
+		tabbedPane.addTab("Panel 2", panel2);
+		////////////////////////////////////
+
+		this.add(tabbedPane);
+
+		this.setVisible(true);
+
+	}
+
+}
---
+++ src/GUI/Login.java
@@ -0,0 +1,55 @@
+package GUI;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import Model.Department;
+
+public class Login extends JFrame {
+
+	private JPanel contentPane;
+	private Department department;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login frame = new Login();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Login(Department department) {
+		this.department = department;
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+	public Login() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
